33aaad6035afffaa693e7aad2fe3e69f905f686d
kaloyanmk <kaloyanmk@gmail.com>
2016-10-17 21:55:34 +0300

    Update project1.php (#8)

--- project1.php
+++ project1.php
@@ -7,7 +7,12 @@
 <body>
   
   <div id="project">This out project!!!</div>
-  
+    +<script type="text/javascript">
+   for(var i=0; i<=20; i++)
+   {
+     document.write(i+" ")
+   }
+ </script>
   </body>
 
 </html>
