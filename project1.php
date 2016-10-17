d4bd0eb1a1711f87dac6b5868aa1f354999655ec
kaloyanmk <kaloyanmk@gmail.com>
2016-10-17 21:57:33 +0300

    Update project1.php

--- project1.php
+++ project1.php
@@ -14,10 +14,11 @@
 </style>
   
   <div id="project">This out project!!!</div>
-    +<script type="text/javascript">
+    <script type="text/javascript">
    for(var i=0; i<=20; i++)
+   //sadfdfsdfsdf
    {
-     document.write(i+" ")
+     document.write(i+" ");
    }
  </script>
   </body>
