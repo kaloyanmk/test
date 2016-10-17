8d4d21b59561afd0f3ed782f0d4210898d4ac4b6
Blago24 <blago990@abv.bg>
2016-10-17 21:42:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+echo "lets get started";
+
+echo $_SESSION['user_name'];
+echo $_SESSION['user_pass'];
+ echo $_SESSION['user_car'];
+  echo$_SESSION['user_lvl']
+  echo$_SESSION['user_money']
+
+?>
